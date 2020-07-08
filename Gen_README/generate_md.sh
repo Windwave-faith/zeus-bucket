ec94db5f7ce1435af3e6f3ce47ca846a6ae7928e
Kaznaami for Private <50604480+Windwave-faith@users.noreply.github.com>
2020-07-08 15:14:40 +0900

    Update generate_md.sh

--- Gen_README/generate_md.sh
+++ Gen_README/generate_md.sh
@@ -15,8 +15,8 @@ COMMIT_MSG="README: Update something"
 function manifest_check(){
   git log --pretty=format:%h -2
   COMMIT_HASH=`git log --pretty=format:%h -2`
-  BEFORE=`echo ${COMMIT_HASH} | cut -d " " -f 0`
-  AFTER=`echo ${COMMIT_HASH} |  cut -d " " -f 1`
+  BEFORE=`echo ${COMMIT_HASH} | cut -d " " -f 1`
+  AFTER=`echo ${COMMIT_HASH} |  cut -d " " -f 2`
   
   git diff $BEFORE $AFTER --relative=bucket --exit-code --name-only
   #git diff HEAD --relative=bucket --exit-code --name-only
